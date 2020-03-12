c964993d068fe6b5d28648b21d2ded6aa7a56426
Damir <g.damir-2000@bk.ru>
2020-03-12 23:08:28 +0300

    1 Task 2 Task in progress

--- 2/index.php
+++ 2/index.php
@@ -1,3 +1,19 @@
 <?php
-include "forma.html";
+//include "forma.html";
+//include "check.php"
+function square($value)
+{
+    yield $value;
+}
+
+function even_square($arr)
+{
+    foreach ($arr as $value) {
+        if ($value % 2 != 0) yield from square($value);
+    }
+}
+
+$arr = [1,2,3,4,5,6];
+foreach (even_square($arr) as $val) echo "$val ";
+
 ?>
